03d8d6aea0a74da442305a222036bdc323696b6c
Rashenshammika <shenwotsan03@gmail.com>
2024-06-19 10:26:59 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+function calc() {
+    
+    let number01 = "";
+    let number02 = "";
+
+    number01 = new Number("12");
+    number02 = new Number("10");
+
+    number01 = new Number(12);
+    number02 = new Number(10);
+
+    console.log(number01+number02);
+
+    console.log(typeof number01);
+    console.log(typeof number02);
+
+}
